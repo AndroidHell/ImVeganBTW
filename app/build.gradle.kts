19c396f599480b75890d37d297552626f9aeb305
derrick <ralkkai1337@gmail.com>
2023-10-18 09:35:31 -0500

    test commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.androidhell.imveganbtw"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
